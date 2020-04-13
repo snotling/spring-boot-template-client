9f7c647ee69e669f98419d58bfc74ff848810fde
snotling54 <guillaume.maljean@gmail.com>
2020-04-13 18:17:40 +0200

    init

---
+++ src/main/java/org/snot/springboottemplate/SpringBootTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.snot.springboottemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTemplateApplication.class, args);
+	}
+
+}
